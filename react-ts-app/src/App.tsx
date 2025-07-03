9f1ec2bcca6bc2e1395a38d7c9624406ca3f1ebb
nhannn87dn <nhannn87dn@gmail.com>
2025-07-03 16:29:51 +0700

    attr

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import ArticlesList from './ArticlesList';
+import Attributes from './Attributes';
 
 const articles = [
   {id: 1, title: 'Ấn tượng đầu tiên Samsung Galaxy A32 4G: Với hơn 6 triệu đã có màn hình Super AMOLED 90Hz', thumbnail: 'images/1.jpeg', addTime: '4 năm trước'},
@@ -8,9 +9,16 @@ const articles = [
   {id: 4, title: 'Galaxy A82 5G chuẩn bị ra mắt với chip flagship và màn hình trượt độc đáo, Samfans gom lúa đi là vừa', thumbnail: 'images/4.jpeg', addTime: '4 năm trước'}
 ]
 
+const attributes = [
+  {id: 1, label: 'Đen'},
+  {id: 2, label: 'Hồng'},
+  {id: 3, label: 'Xanh'}
+]
+
 function App() {
   return (
     <main className='container'>
+      <Attributes data={attributes} />
      <section className='section'>
         <div className="section_header">
             <h2 className="section_title">Tin tức</h2>
