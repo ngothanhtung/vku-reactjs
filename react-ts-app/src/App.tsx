3f151de77a49c9266b459024978c491be9c2a657
nhannn87dn <nhannn87dn@gmail.com>
2025-07-08 16:24:51 +0700

    homeworks day7

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,37 +1,11 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router";
-import DefaultLayout from './layouts/DefaultLayout';
-import HomePage from './pages/HomePage';
-import BlogPage from './pages/BlogPage';
-import CategoryPage from './pages/CategoryPage';
-import NoPage from './pages/NoPage';
-import ProductPage from './pages/ProductPage';
-import ProductDetailsPage from './pages/ProductDetailsPage';
-import LoginPage from './pages/LoginPage';
-import CustomerDashboard from './pages/CustomerDashboard';
-import CustomerProfile from './pages/CustomerProfile';
-import CustomerLayout from './layouts/CustomerLayout';
-import CustomerOrders from './pages/CustomerOrders';
+import DashboardApp from './Lession07-Guidelines/DashboardApp';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DefaultLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="blog" element={<BlogPage />} />
-          <Route path="category" element={<CategoryPage />} />
-          <Route path="products" element={<ProductPage />} />
-          <Route path="products/:slug" element={<ProductDetailsPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="customer/" element={<CustomerLayout />}>
-               <Route index element={<CustomerDashboard />} />
-               <Route path='orders' element={<CustomerOrders />} />
-               <Route path='profile' element={<CustomerProfile />} />
-          </Route>
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+    <DashboardApp />
+    </>
   );
 }
 
---
+++ react-ts-app/src/Lession07-Guidelines/pages/MapPage.tsx
@@ -0,0 +1,7 @@
+const MapPage = () => (
+  <div className="p-6">
+    <h1 className="text-2xl font-semibold text-gray-800">Map Page</h1>
+  </div>
+);
+
+export default MapPage;
