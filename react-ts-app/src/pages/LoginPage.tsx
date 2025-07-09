32421148a21a5974ffd6f8063d42eec007853e30
Ngo Thanh Tung <tungnt@softech.vn>
2025-07-10 00:28:31 +0700

    feat: add signup form component with validation

--- react-ts-app/src/pages/LoginPage.tsx
+++ react-ts-app/src/pages/LoginPage.tsx
@@ -1,11 +1,11 @@
-import Form01 from "../Lesson09-Guidlines/Form01SignUp"
+import Form01 from '../Lesson09-Guidelines/Form01SignUp';
 
 const LoginPage = () => {
   return (
     <div className="container mx-auto">
-      <Form01/>
+      <Form01 />
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
