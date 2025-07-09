3929f15ff77dd2c9ae3a94b9d7a5d60ffed3eaa3
Ngo Thanh Tung <tungnt@softech.vn>
2025-07-09 07:15:05 +0700

    feat: integrate Tailwind CSS into the React TypeScript application
    - Un-commented the Tailwind CSS link in index.html for styling.
    - Added Tailwind CSS and its Vite plugin to package.json dependencies.
    - Updated App.tsx to include Tailwind CSS classes and reorganized imports.
    - Modified index.css to import Tailwind CSS.
    - Updated vite.config.ts to include the Tailwind CSS Vite plugin.
    - Updated yarn.lock to reflect the new dependencies and their versions.

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,32 +1,25 @@
-// import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import './App.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+import CustomerLayout from './layouts/CustomerLayout';
 import DefaultLayout from './layouts/DefaultLayout';
-import HomePage from './pages/HomePage';
 import BlogPage from './pages/BlogPage';
 import CategoryPage from './pages/CategoryPage';
-import NoPage from './pages/NoPage';
-import ProductPage from './pages/ProductPage';
-import ProductDetailsPage from './pages/ProductDetailsPage';
-import LoginPage from './pages/LoginPage';
 import CustomerDashboard from './pages/CustomerDashboard';
-import CustomerProfile from './pages/CustomerProfile';
-import CustomerLayout from './layouts/CustomerLayout';
 import CustomerOrders from './pages/CustomerOrders';
+import CustomerProfile from './pages/CustomerProfile';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import NoPage from './pages/NoPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import ProductPage from './pages/ProductPage';
 
-import { Button, HStack } from '@chakra-ui/react';
-import { RiArrowRightLine, RiMailLine } from 'react-icons/ri';
 function App() {
   return (
     <BrowserRouter>
-      <HStack>
-        <Button variant="solid">
-          <RiMailLine /> Email
-        </Button>
-        <Button loading loadingText="Loading" spinnerPlacement="end" colorPalette={'orange'} variant="surface">
-          Call us <RiArrowRightLine />
-        </Button>
-      </HStack>
-      {/* <Routes>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<HomePage />} />
           <Route path="blog" element={<BlogPage />} />
@@ -41,7 +34,7 @@ function App() {
           </Route>
           <Route path="*" element={<NoPage />} />
         </Route>
-      </Routes> */}
+      </Routes>
     </BrowserRouter>
   );
 }
