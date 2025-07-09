4fa1063fb91236267475bfc1da1c2353701a1983
nhannn87dn <nhannn87dn@gmail.com>
2025-07-09 15:14:29 +0700

    update

--- react-ts-app-deploy/src/pages/LoginPage.tsx
+++ react-ts-app-deploy/src/pages/LoginPage.tsx
@@ -1,9 +1,11 @@
 import Form01SignUp from "../components/Lesson09-Guidlines/Form01SignUp"
+import Form02Register from "../components/Lesson09-Guidlines/Form02Register"
 
 const LoginPage = () => {
   return (
     <div>
       <Form01SignUp />
+      <Form02Register />
     </div>
   )
 }
