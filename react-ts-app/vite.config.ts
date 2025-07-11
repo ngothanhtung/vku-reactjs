336bea654e00db189c25ea773619bd608aad117e
Ngo Thanh Tung <tungnt@softech.vn>
2025-07-11 08:16:55 +0700

    Refactor code structure for improved readability and maintainability

--- react-ts-app/vite.config.ts
+++ react-ts-app/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -7,4 +8,13 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
