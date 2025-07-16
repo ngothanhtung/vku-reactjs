4d9ff79575d81e54d362fc1051f607e9867b9fcc
Ngo Thanh Tung <tungnt@softech.vn>
2025-07-16 15:06:24 +0700

    Refactor code structure for improved readability and maintainability

--- nextjs-ts-app/src/app/shop/layout.tsx
+++ nextjs-ts-app/src/app/shop/layout.tsx
@@ -7,9 +7,9 @@ type Props = {
 export default function layout({ children }: Props) {
   return (
     <div>
-      <h1>Shop Layout</h1>
-      <hr />
-      <div style={{ backgroundColor: 'orange' }}> {children}</div>
+      <h1 className='text-4xl font-bold'>Shop</h1>
+      <hr className='border-t border-gray-300 my-[20px]' />
+      <div style={{}}> {children}</div>
     </div>
   );
 }
