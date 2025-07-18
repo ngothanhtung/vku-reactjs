975b83abd9cbb895c195190fc266f9f7475fae0d
Ngo Thanh Tung <tungnt@softech.vn>
2025-07-18 15:00:17 +0700

    refactor: remove unused import of NavigateFunction from useAuthStore

--- react-ts-app/src/Lesson13/useAuthStore.ts
+++ react-ts-app/src/Lesson13/useAuthStore.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { NavigateFunction } from 'react-router';
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 
