b7f2a75edd4129904cd11fdac4586428c5047df9
Ngo Thanh Tung <tungnt@softech.vn>
2025-08-28 13:29:33 +0700

    feat: update Lottie animation source in LottieExample component

--- react-ts-app/src/Bonus-Lotties/index.tsx
+++ react-ts-app/src/Bonus-Lotties/index.tsx
@@ -5,7 +5,7 @@ type Props = {};
 export default function LottieExample({}: Props) {
   return (
     <div>
-      <DotLottieReact src="/public/assets/lotties/teaching.json" loop autoplay />
+      <DotLottieReact src="https://assets-v2.lottiefiles.com/a/3f907b04-8f0c-4483-a83f-59fd009b55d8/nKnq6P2G7R.lottie" loop autoplay />
 
       {/* <img src={'/public/assets/vectors/education.svg'} /> */}
     </div>
