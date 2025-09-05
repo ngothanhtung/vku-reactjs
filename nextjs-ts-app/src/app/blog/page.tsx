d8ddd7f044f35e8456d9288d72e8b52973721910
Ngo Thanh Tung <tungnt@softech.vn>
2025-09-05 08:41:15 +0700

    Add Lighthouse CI configuration and create basic blog and contact pages
    - Implemented Lighthouse CI workflow in GitHub Actions to run performance audits on push and pull request events.
    - Configured Lighthouse CI settings in `.lighthouserc.json` with performance assertions and URLs to test.
    - Created a blog page with navigation links to Home and Contact.
    - Created a contact page with navigation links to Home and Blog.

--- nextjs-ts-app/src/app/blog/page.tsx
+++ nextjs-ts-app/src/app/blog/page.tsx
@@ -5,11 +5,7 @@ export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-row gap-[12px] row-start-2 items-center sm:items-start'>
-        <Link href='/shop'>Shop</Link>
-        {' | '}
-        <Link href='/administration'>Administration</Link>
-        {' | '}
-        <Link href='/blog'>Blog</Link>
+        <Link href='/'>Home</Link>
         {' | '}
         <Link href='/contact'>Contact</Link>
       </main>
